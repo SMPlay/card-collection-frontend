c048cc34a794b40c4eec21f25deabf0cdf4d84ab
SMPlay <pavlik.sokolov.2000@gmail.com>
2020-07-13 23:30:03 +0300

    added list of collections

--- src/components/Navbar/Nav-item.tsx
+++ src/components/Navbar/Nav-item.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { createUseStyles, useTheme } from "react-jss";
 
+import arrowImg from "../../img/arrow.png";
+
 const useStyles = createUseStyles({
   navItem: {
     position: "relative",
     fontSize: 22,
     fontWeight: 600,
-    maxWidth: 130,
+    width: 150,
     height: 27,
     padding: "0 10px",
     color: "rgb(255, 255, 255)",
@@ -28,9 +30,7 @@ const useStyles = createUseStyles({
       transition: "opacity .2s ease",
     },
     "&:hover": {
-      "&::after": {
-        opacity: .6
-      }
+      "&::after": { opacity: .6 }
     }
   },
   navItemChild: {
@@ -40,16 +40,38 @@ const useStyles = createUseStyles({
     justifyContent: "center",
     alignItems: "center",
     transform: "skew(20deg)",
-  }
+  },
+  arrow: {
+    position: "absolute",
+    top: 5,
+    right: -7,
+    transform: "rotate(-90deg)",
+    transition: "transform .2s ease",
+    "&.open": {
+      transform: "rotate(0)"
+    }
+  },
 });
 
-export const NavItem: React.FC = ({ children }) => {
+interface NavItemProps {
+  isManyThings?: boolean;
+  isOpen?: boolean;
+}
+
+export const NavItem: React.FC<NavItemProps> = ({ children, isManyThings, isOpen }) => {
   const theme = useTheme();
   const styles = useStyles({theme});
 
   return (
     <div className={styles.navItem}>
-      <span className={styles.navItemChild}>{children}</span>
+      <span className={styles.navItemChild}>
+        {children}
+        {isManyThings && 
+        <img 
+          src={arrowImg} 
+          alt="right arrow" 
+          className={`${styles.arrow} ${isOpen ? "open" : ""}`}/>}
+      </span>
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,113 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { createUseStyles } from "react-jss";
+import { createUseStyles, useTheme } from "react-jss";
 
 import { NavItem } from "./Nav-item";
 
 const useStyles = createUseStyles({
-  nav: {
-    display: "flex"
-  },
+  nav: { display: "flex" },
   navItem: {
-    textDecoration: "none"
-  }
+    position: "relative",
+    textDecoration: "none",
+    fontSize: 14,
+  },
+  navDetails: {
+    position: "absolute",
+    padding: "10px 0",
+    margin: 0,
+    border: "1px solid rgb(18, 18, 18)",
+    width: 250,
+    top: 0,
+    borderRadius: 3,
+    zIndex: -5,
+    listStyleType: "none",
+    opacity: 0,
+    background: ({theme}) => theme.colorPrimary,
+    transition: "all .2s ease-in-out"
+  },
+  navDetailsItem: {
+    position: "relative",
+    padding: "0 13px",
+    "&:not(:first-child)": {
+      marginTop: 15,
+      "&::before": {
+        position: "absolute",
+        content: "''",
+        width: "100%",
+        height: 2,
+        top: -7,
+        background: "linear-gradient(to right, rgba(247, 247, 247,0) 0%,rgba(247, 247, 247, 0.98) 50%,rgba(247, 247, 247,1) 51%,rgba(247, 247, 247,0) 99%,rgba(247, 247, 247,0) 100%)"
+      }
+    },
+    "&::after": {
+      position: "absolute",
+      top: 8,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      content: "''",
+      width: "100%",
+      height: "110%",
+      opacity: 0,
+      background: "linear-gradient(to top, rgba(255, 255, 255, .5), transparent)",
+      transition: "opacity .2s ease",
+    },
+    "&:hover": {
+      "&::after": { opacity: .6 },
+    },
+    "& a": { color: "#fff" }
+  },
+  collectionsItem: {
+    "&:hover": {
+      "& $navDetails": {
+        opacity: 1,
+        top: "100%",
+      }
+    }
+  },
 });
 
-export const Navbar = () => {
-  const styles = useStyles()
+export const Navbar: React.FC = () => {
+  const theme = useTheme();
+  const styles = useStyles({theme});
 
   return (
     <nav className={styles.nav}>
       <Link to="/" className={styles.navItem}>
         <NavItem>Главная</NavItem>
       </Link>
-      <Link to="/collections" className={styles.navItem}>
-        <NavItem>Коллекции</NavItem>
+      <Link to="/collections" className={`${styles.navItem} ${styles.collectionsItem}`}>
+        <NavItem
+          isManyThings
+          >Коллекции</NavItem>
+        <ul 
+          className={styles.navDetails}
+          >
+          <li className={styles.navDetailsItem}>
+            <Link
+              to="/collections/spider-man-part-1"
+              className={styles.navItem}
+            >
+              Человек-Паук: Герои и злодеи часть 1
+            </Link>
+          </li>
+          <li className={styles.navDetailsItem}>
+            <Link
+              to="/collections/spider-man-part-2"
+              className={styles.navItem}
+            >
+              Человек-Паук: Герои и злодеи часть 2
+            </Link>
+          </li>
+          <li className={styles.navDetailsItem}>
+            <Link
+              to="/collections/spider-man-part-3"
+              className={styles.navItem}
+            >
+              Человек-Паук: Герои и злодеи часть 3
+            </Link>
+          </li>
+        </ul>
       </Link>
       <Link to="/gallery" className={styles.navItem}>
         <NavItem>Галерея</NavItem>
