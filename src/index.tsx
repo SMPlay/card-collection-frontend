6a66b02ce4e2650d8b561e5c830aceacc0f19269
SMPlay <pavlik.sokolov.2000@gmail.com>
2020-07-14 13:53:14 +0300

    added apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import App from './components/App';
+import './index.css';
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080"
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
