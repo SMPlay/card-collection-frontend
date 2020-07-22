b6129cc8431bef19950052dfe88bd2397a560f99
SMPlay <pavlik.sokolov.2000@gmail.com>
2020-07-22 16:40:52 +0300

    added prototype registration form

--- src/pages/registration-page.tsx
+++ src/pages/registration-page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Container } from "@material-ui/core";
+
+import { RegistrationForm } from "../components";
 
 export const RegistrationPage: React.FC = () => {
   return (
-    <main></main>
+    <main>
+      <Container>
+        <RegistrationForm/>
+      </Container>
+    </main>
   );
 }
